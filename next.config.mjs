6b13957cdbe2f44ae46da4f9ea984020b095fe5f
Julia Paul <juliapaulmyla@yahoo.com>
2024-03-19 21:41:42 -0400

    yml files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    basePath: '/to-do-app',
+    output: "export",  
+    reactStrictMode: true,
 };
 
 export default nextConfig;
